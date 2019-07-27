d100dd92f2979b3a6385c4a2d5d768bb07e54b10
Robert Hubert <robertfhubert@gmail.com>
2019-07-27 14:36:38 -0700

    nodemon

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import * as d3 from "d3";
 import * as topojson from "topojson";
 
+const express = require("express");
+const app = express();
 
+app.use(express.static(__dirname + '/public'));
+// app.use("/assets/data", express.static(__dirname + '/assets/data'));
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+const port = process.env.PORT || 5001;
+
+app.listen(port, () => { console.log(`Server is running on port ${port}`) });
  
 let state;
 
