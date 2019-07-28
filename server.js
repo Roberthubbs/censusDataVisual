69e729fd694dbf1c2c13ec2bcef11076b57c4fcd
Robert Hubert <robertfhubert@gmail.com>
2019-07-28 11:52:50 -0700

    package

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// const favicon = require("serve-favicon");
+
 const path = require('path');
 const express = require("express");
 const app = express();
@@ -9,7 +9,6 @@ server.listen(port, () => {
 });
 
 app.use(express.static(__dirname + "/dist"));
-// app.use("/assets/data", express.static(__dirname + '/assets/data'));
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
