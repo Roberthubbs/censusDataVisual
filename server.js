e18651eefed1767701a9a09a6b7c20952f790fd4
Robert Hubert <robertfhubert@gmail.com>
2019-07-29 09:25:57 -0700

    fav

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const path = require('path');
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
+
 let server = require('http').Server(app);
+
+
 server.listen(port, () => {
     console.log("App is running on port " + port);
 });
