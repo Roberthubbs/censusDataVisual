e1a7be6dd18e8b826db55d73f2a52c3639d9c38b
Robert Hubert <robertfhubert@gmail.com>
2019-08-05 10:47:37 -0700

    styling

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,7 @@ const geo = require("d3-geo")
 
 
 document.addEventListener('DOMContentLoaded', () => {
-{
+
     // let state;  
     
     
@@ -24,60 +24,6 @@ let svg = d3.select("svg")
 let path = d3.geoPath();
 
 
-// d3.json("https://d3js.org/us-10m.v1.json").then( (us) =>  {
-    
-    
-//     // if (error) throw error;
-    
-//     svg.append("g")
-//     .attr("class", "states")
-//     .selectAll("path")
-//     .data(topojson.feature(us, us.objects.states).features)
-//     .enter().append("path")
-//     .attr("d", path);
-    
-    
-//     svg.append("path")
-//     .attr("class", "state-borders")
-//     .attr("d", path(topojson.mesh(us, us.objects.states, function (a, b) { return a !== b; })));
 
-// });
-
-}
-{
-//     debugger;
-//     // d3.json(us).then((data) => {
-//         // debugger;
-//     var width = 960,
-//         height = 500;
-
-//     var svg = d3.select('body').append('svg')
-//         .attr('width', width)
-//         .attr('height', height);
-
-//     var path = d3.geoPath()
-//         .projection(projection);
-    
-//     var projection = d3.geoAlbersUsa()
-//         .scale(1000)
-//         .translate([width / 2, height / 2]);
-        
-
-//     d3.json(us, function (error, us) {
-//         svg.selectAll('.states')
-//             .data(topojson.feature(us, us.objects.usStates).features)
-//             .enter()
-//             .append('path')
-//             .attr('class', 'states')
-//             .attr('d', path)
-//             .on('mouseover', function (d) {
-//                 var name = d.properties.STATE_ABBR;
-//                 return document.getElementById('name').innerHTML = name;
-//             });
-//     });  
-}
-  
-
-    // });
 });
 
